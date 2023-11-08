a4592c0f993be9fdb961c65dbf6c2fe8763e28ca
abdessamad-zgor <abdessamadzgor@gmail.com>
2023-11-08 23:12:46 +0100

    comments productform

--- src/components/admin/productform.svelte
+++ src/components/admin/productform.svelte
@@ -29,10 +29,13 @@
 </script>
 
 <form class="md:w-3/5 m-auto bg-white flex flex-col rounded-xl py-4 px-8 border shadow-lg " action="" on:submit={handleSubmit}>
+  <!--Titre-->
   <div class="py-px">   
     <label class="font-light text-stone-600" for="">Titre</label>
     <input type="text" class="w-full" bind:value={produit.titre}>
   </div>
+  <!---->
+  <!--Slug-->
   <div class="py-px">   
     <label class="font-light text-stone-600" for="">Slug</label>
     <div class="flex flex-row gap-2">
@@ -40,7 +43,9 @@
       <button on:click={generateSlug} class="basis-1/5 border rounded-lg py-[2px] border-stone-200 text-zinc-500">genrate-slug</button>
     </div>
   </div>
+  <!---->
   <div class="flex flex-row gap-2 items-center py-px">
+    <!--Type-->
     <div class="basis-1/2">   
       <label class="font-light text-stone-600" for="">Type</label>
       <select bind:value={produit.type} class="w-full p-2">
@@ -49,6 +54,8 @@
         {/each}
       </select>
     </div>
+    <!---->
+    <!--Solubilite-->
     <div class="basis-1/2">
       <label  class="font-light text-stone-600" for="">Solubilité</label>
       <select multiple bind:value={produit.solubilite} class="w-full p-2">
@@ -57,7 +64,9 @@
         {/each}
       </select>
     </div>
+    <!---->
   </div>
+  <!--Varients-->
   <div>   
     <label class="font-light text-stone-600" for="">Varients</label>
     <div class="flex mb-2 font-thin py-px flex-row bg-gradient-to-b from-stone-200 to-zinc-300 text-sky-900 px-4 border-b-2 border-sky-900">
@@ -82,6 +91,8 @@
       <button class="px-4 py-2 bg-zinc-600 text-white rounded-lg" on:click={addVariant}>ajouter varient</button>
     </div>
   </div>
+  <!---->
+  <!--Proprietes-->
   <div>
     <label class="mb-2 font-light text-stone-600" for="">Proprietes</label>
     {#each produit.properties as propriete, i }
@@ -94,19 +105,22 @@
       <button on:click={addProperty} class="rounded-lg py-2 px-4 bg-zinc-600 text-white">ajouter propriete</button>
     </div>
   </div>
-  <div >
-    <label for="" class=""></label>
-  </div>
+  <!---->
   <div class="flex flex-row gap-4 py-2">
+    <!--Designation-->
     <div class="basis-1/2">
       <label class="font-light text-stone-600" for="">Désignation INCI</label>
       <input type="text" bind:value={produit.inci} class="w-full">
     </div>
+    <!---->
+    <!--Origine-->
     <div class="basis-1/2">
       <label class="font-light text-stone-600" for="">Origine</label>
       <input type="text" bind:value={produit.origin} class="w-full">
     </div>
+    <!---->
   </div>
+  <!--Utilisations-->
   <div>
     <label class="font-light text-stone-600" for="">Utilisations</label>
     {#each produit.utilisations as util, i}
@@ -124,6 +138,8 @@
       <button on:click={addUse} class="rounded-lg py-2 px-4 bg-zinc-600 text-white">ajouter utilisation</button>
     </div>
   </div>
+  <!---->
+  <!--Description-->
   <div class="py-2">
     <label class="font-light text-stone-600" for="">Description</label>
     <textarea name="" style="resize: none;" bind:value={produit.description} class="rounded-lg border border-stone-500 w-full"></textarea>
@@ -132,6 +148,7 @@
     <input type="submit" value="Annuler" class="basis-1/4 px-4 rounded py-2 text-white bg-stone-600 shadow">
     <input type="submit" value="Enregistrer" class="basis-1/4 px-4 rounded py-2 text-white bg-sky-600 shadow">
   </div>
+  <!---->
 </form>
 
 <style>
